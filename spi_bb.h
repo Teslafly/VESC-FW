389fdf46a4d1b66b5d0337823b30fcf155f9a102
Teslafly <2079881+Teslafly@users.noreply.github.com>
2022-02-22 20:57:09 -0600

    revert spi_bb fiiles

--- spi_bb.h
+++ spi_bb.h
@@ -25,13 +25,6 @@
 #include "stdint.h"
 #include "stdbool.h"
 
-enum spi_types{
-	sw_spi, // spi = seperate mosi and miso wires
-	hw_spi,
-	sw_ssc, // ssc = one data wire using mosi pin
-	hw_ssc,
-	};
-
 typedef struct {
 	stm32_gpio_t *nss_gpio;
 	int nss_pin;
@@ -41,28 +34,16 @@ typedef struct {
 	int mosi_pin;
     stm32_gpio_t *miso_gpio;
 	int miso_pin;
-	enum spi_types spi_type;
 	bool has_started;
 	bool has_error;
 	mutex_t mutex;
 } spi_bb_state;
 
 void spi_bb_init(spi_bb_state *s);
-void spi_bb_deinit(spi_bb_state *s);
 uint8_t spi_bb_exchange_8(spi_bb_state *s, uint8_t x);
 void spi_bb_transfer_8(spi_bb_state *s, uint8_t *in_buf, const uint8_t *out_buf, int length);
-// void spi_bb_transfer_16(spi_bb_state *s, uint16_t *in_buf, const uint16_t *out_buf, int length);
 void spi_bb_begin(spi_bb_state *s);
 void spi_bb_end(spi_bb_state *s);
 void spi_bb_delay(void);
-
-void ssc_bb_init(spi_bb_state *s);
-void ssc_bb_deinit(spi_bb_state *s);
-// void ssc_bb_begin(spi_bb_state *s);
-// void ssc_bb_end(spi_bb_state *s);
-// void ssc_bb_transfer_16(spi_bb_state *s, uint16_t *in_buf,
-// 		const uint16_t *out_buf, int length, bool write);
-
-bool spi_bb_check_parity(uint16_t x);
 
 #endif /* SPI_BB_H_ */
