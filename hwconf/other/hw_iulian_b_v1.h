17d8081446db06816a95899694af03903d46caad
Teslafly <2079881+Teslafly@users.noreply.github.com>
2023-03-23 20:04:06 -0500

    update i2c pins

--- hwconf/other/hw_iulian_b_v1.h
+++ hwconf/other/hw_iulian_b_v1.h
@@ -192,9 +192,9 @@
 #define HW_I2C_DEV				I2CD2
 #define HW_I2C_GPIO_AF			GPIO_AF_I2C2
 #define HW_I2C_SCL_PORT			GPIOB
-#define HW_I2C_SCL_PIN			10
-#define HW_I2C_SDA_PORT			GPIOB
-#define HW_I2C_SDA_PIN			11
+#define HW_I2C_SCL_PIN			2
+#define HW_I2C_SDA_PORT			GPIOA
+#define HW_I2C_SDA_PIN			15
 
 // Hall/encoder pins
 #define HW_HALL_ENC_GPIO1		GPIOC
